52140111fd0a3ea90f3af491fdce35247d1ec184
xchen1 <xchen1@tcl.com>
2020-03-09 11:24:01 +0800

    add eureka

--- demo01/src/main/java/com/test/demo01/Demo01Application.java
+++ demo01/src/main/java/com/test/demo01/Demo01Application.java
@@ -2,8 +2,11 @@ package com.test.demo01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Demo01Application {
 
     public static void main(String[] args) {
